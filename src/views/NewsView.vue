24e68772630522ad0e92caf0ef668f8ca791c7e2
jishu <jishuya3015@gmail.com>
2022-03-07 20:39:46 +0900

    axios added

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        News
+        <div v-for="user in users" :key="user">{{ user.title }}</div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        var vm = this;
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+          .then(function(respose) {
+            console.log(respose)
+            vm.users = respose.data;
+          })
+          .catch(function(error) {
+            console.log(error);
+          });    
+    },
 }
 </script>
 
