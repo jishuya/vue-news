748ae26347b96bef8e24e81e0c8ccf77ad1876b4
jslim <71686380+jishuya@users.noreply.github.com>
2022-06-09 05:24:47 +0900

    css updated

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,9 +1,23 @@
 <template>
     <div>
-        <p v-for="(item, index) in fetchedAsk" :key="index">
+        <ul class="ask-list">
+            <li v-for="(item, index) in fetchedAsk" :key="index">
+                <!-- 포인트영역 -->
+                <div class="points">
+                    {{ item.points }}
+                </div>
+                <!-- 기타정보 영역 -->
+                <div>
+                    <p class="ask-title">
+                        <!-- <a :href=""></a> -->
+                    </p>
+                </div>
+            </li>
+        </ul>
+        <!-- <p v-for="(item, index) in fetchedAsk" :key="index">
             <router-link :to="`item/${item.id}`">{{ item.title }}</router-link>
             <small>{{ item.time_ago }} by {{ item.user }}</small>
-        </p>
+        </p> -->
     </div>
 </template>
 
